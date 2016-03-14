5342a100fcf0b43dc0255f4e518e05603c8b2c44
Guillermo Bernal <gbernal096@gmail.com>
2016-03-13 20:07:40 -0500

    blood types as constant

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -3,7 +3,7 @@
 
 angular.module('matriculasApp.constants', [])
 
-.constant('appConfig', {userRoles:['usuario','secretaria','administrador'],nivelTitulo:['Primer nivel','Segundo nivel','Tercer nivel','Cuarto nivel'],nivelesDisponibles:['Inicial 2(3 años)','Inicial 2(4 años)','Primero de básica','Segundo de básica','Tercero de básica','Cuarto de básica','Quinto de básica','Sexto de básica','Séptimo de básica']})
+.constant('appConfig', {userRoles:['usuario','secretaria','administrador'],nivelTitulo:['Primer nivel','Segundo nivel','Tercer nivel','Cuarto nivel'],nivelesDisponibles:['Inicial 2(3 años)','Inicial 2(4 años)','Primero de básica','Segundo de básica','Tercero de básica','Cuarto de básica','Quinto de básica','Sexto de básica','Séptimo de básica'],tiposSangre:['O negativo','O positivo','A negativo','A positivo','B negativo','B positivo','AB negativo','AB positivo']})
 
 ;
 })(angular);
